48aa7292bd8735665b681120ca777e7fa2d8bbb6
Richard Hill <richardhill101@gmail.com>
2017-11-20 12:59:42 +0000

    First commit

---
+++ DynamicStyles/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DynamicStyles
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool done = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            done = false;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () => { 
+                Resources["now"] = DateTime.Now.ToString();
+                return !done;
+            });
+
+            base.OnAppearing();
+        }
+        protected override void OnDisappearing()
+        {
+            done = true;
+
+            base.OnDisappearing();
+        }
+    }
+}
